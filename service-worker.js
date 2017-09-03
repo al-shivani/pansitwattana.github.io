5636a05bc700d3f8233325f7d089f973882e9726
pansitwattana <pansit.wt@gmail.com>
2017-09-03 20:31:33 +0700

    add service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,120 @@
-"use strict";function setOfCachedUrls(e){return e.keys().then(function(e){return e.map(function(e){return e.url})}).then(function(e){return new Set(e)})}var precacheConfig=[["/index.html","9a2c86a652ab13b52e76af6a623b6129"],["/static/css/main.9ed6c276.css","a56a563f2e0b3147ff09b9f848c9ef3a"],["/static/js/main.13ec03e7.js","759f7b38a4034bd4a24a33d667154a9a"],["/static/media/Symbola.4621fcfd.otf","4621fcfd9def63c694914f7ec5add610"],["/static/media/Symbola.52a6aac1.ttf","52a6aac18ae26b6ecbd4f3a0d9579c9f"],["/static/media/Symbola.b1445a46.woff","b1445a46ceac48f13cec0860ab1acf5f"],["/static/media/Symbola.cb8d804a.woff2","cb8d804a242b86175fdd6cb8e11b1a35"],["/static/media/Symbola.e4ae9ff7.eot","e4ae9ff7ac2476ae421fc4278e5d3806"]],cacheName="sw-precache-v3-sw-precache-webpack-plugin-"+(self.registration?self.registration.scope:""),ignoreUrlParametersMatching=[/^utm_/],addDirectoryIndex=function(e,t){var n=new URL(e);return"/"===n.pathname.slice(-1)&&(n.pathname+=t),n.toString()},cleanResponse=function(e){return e.redirected?("body"in e?Promise.resolve(e.body):e.blob()).then(function(t){return new Response(t,{headers:e.headers,status:e.status,statusText:e.statusText})}):Promise.resolve(e)},createCacheKey=function(e,t,n,a){var r=new URL(e);return a&&r.pathname.match(a)||(r.search+=(r.search?"&":"")+encodeURIComponent(t)+"="+encodeURIComponent(n)),r.toString()},isPathWhitelisted=function(e,t){if(0===e.length)return!0;var n=new URL(t).pathname;return e.some(function(e){return n.match(e)})},stripIgnoredUrlParameters=function(e,t){var n=new URL(e);return n.hash="",n.search=n.search.slice(1).split("&").map(function(e){return e.split("=")}).filter(function(e){return t.every(function(t){return!t.test(e[0])})}).map(function(e){return e.join("=")}).join("&"),n.toString()},hashParamName="_sw-precache",urlsToCacheKeys=new Map(precacheConfig.map(function(e){var t=e[0],n=e[1],a=new URL(t,self.location),r=createCacheKey(a,hashParamName,n,/\.\w{8}\./);return[a.toString(),r]}));self.addEventListener("install",function(e){e.waitUntil(caches.open(cacheName).then(function(e){return setOfCachedUrls(e).then(function(t){return Promise.all(Array.from(urlsToCacheKeys.values()).map(function(n){if(!t.has(n)){var a=new Request(n,{credentials:"same-origin"});return fetch(a).then(function(t){if(!t.ok)throw new Error("Request for "+n+" returned a response with status "+t.status);return cleanResponse(t).then(function(t){return e.put(n,t)})})}}))})}).then(function(){return self.skipWaiting()}))}),self.addEventListener("activate",function(e){var t=new Set(urlsToCacheKeys.values());e.waitUntil(caches.open(cacheName).then(function(e){return e.keys().then(function(n){return Promise.all(n.map(function(n){if(!t.has(n.url))return e.delete(n)}))})}).then(function(){return self.clients.claim()}))}),self.addEventListener("fetch",function(e){if("GET"===e.request.method){var t,n=stripIgnoredUrlParameters(e.request.url,ignoreUrlParametersMatching);(t=urlsToCacheKeys.has(n))||(n=addDirectoryIndex(n,"index.html"),t=urlsToCacheKeys.has(n));!t&&"navigate"===e.request.mode&&isPathWhitelisted(["^(?!\\/__).*"],e.request.url)&&(n=new URL("/index.html",self.location).toString(),t=urlsToCacheKeys.has(n)),t&&e.respondWith(caches.open(cacheName).then(function(e){return e.match(urlsToCacheKeys.get(n)).then(function(e){if(e)return e;throw Error("The cached response that was expected is missing.")})}).catch(function(t){return console.warn('Couldn\'t serve response for "%s" from cache: %O',e.request.url,t),fetch(e.request)}))}});
+importScripts('workbox-sw.prod.v2.0.0.js');
+
+/**
+ * DO NOT EDIT THE FILE MANIFEST ENTRY
+ *
+ * The method precache() does the following:
+ * 1. Cache URLs in the manifest to a local cache.
+ * 2. When a network request is made for any of these URLs the response
+ *    will ALWAYS comes from the cache, NEVER the network.
+ * 3. When the service worker changes ONLY assets with a revision change are
+ *    updated, old cache entries are left as is.
+ *
+ * By changing the file manifest manually, your users may end up not receiving
+ * new versions of files because the revision hasn't changed.
+ *
+ * Please use workbox-build or some other tool / approach to generate the file
+ * manifest which accounts for changes to local files and update the revision
+ * accordingly.
+ */
+const fileManifest = [
+  {
+    "url": "asset-manifest.json",
+    "revision": "161be75335f4b55b627478a7526502d8"
+  },
+  {
+    "url": "index.html",
+    "revision": "e2ae2ddcc22bac632f793da1d9d1021a"
+  },
+  {
+    "url": "logo.png",
+    "revision": "02de8dbbb281ac5e7d82eeef724f72b1"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "3b8ffc788323e2f158c5eded6d23d6c9"
+  },
+  {
+    "url": "mathquill/font\\Symbola-basic.eot",
+    "revision": "8df3e41fee3876159737c311ed9b9642"
+  },
+  {
+    "url": "mathquill/font\\Symbola-basic.ttf",
+    "revision": "c91d1d52217bb3429936c002ef6df9ed"
+  },
+  {
+    "url": "mathquill/font\\Symbola-basic.woff",
+    "revision": "d9f7c9e7d73b84f16563abacddbf1482"
+  },
+  {
+    "url": "mathquill/font\\Symbola-basic.woff2",
+    "revision": "5e4d2a2f191cea67bcb285576286adbf"
+  },
+  {
+    "url": "mathquill/font\\Symbola.eot",
+    "revision": "e4ae9ff7ac2476ae421fc4278e5d3806"
+  },
+  {
+    "url": "mathquill/font\\Symbola.otf",
+    "revision": "4621fcfd9def63c694914f7ec5add610"
+  },
+  {
+    "url": "mathquill/font\\Symbola.ttf",
+    "revision": "52a6aac18ae26b6ecbd4f3a0d9579c9f"
+  },
+  {
+    "url": "mathquill/font\\Symbola.woff",
+    "revision": "72981090d0240678c5d0a964fe29f082"
+  },
+  {
+    "url": "mathquill/font\\Symbola.woff2",
+    "revision": "2bb0421d8c622fb2aa1632b099807402"
+  },
+  {
+    "url": "mathquill/mathquill.css",
+    "revision": "d644a8d6117d9552946243a76bcae04b"
+  },
+  {
+    "url": "mathquill/mathquill.js",
+    "revision": "b96a12cc1530d084417bd4b4c050f6e3"
+  },
+  {
+    "url": "static/css\\main.9ed6c276.css",
+    "revision": "a56a563f2e0b3147ff09b9f848c9ef3a"
+  },
+  {
+    "url": "static/css\\main.9ed6c276.css.map",
+    "revision": "cb6782d541fc5930ac217cd91ba290ff"
+  },
+  {
+    "url": "static/js\\main.13ec03e7.js",
+    "revision": "759f7b38a4034bd4a24a33d667154a9a"
+  },
+  {
+    "url": "static/media\\Symbola.4621fcfd.otf",
+    "revision": "4621fcfd9def63c694914f7ec5add610"
+  },
+  {
+    "url": "static/media\\Symbola.52a6aac1.ttf",
+    "revision": "52a6aac18ae26b6ecbd4f3a0d9579c9f"
+  },
+  {
+    "url": "static/media\\Symbola.b1445a46.woff",
+    "revision": "b1445a46ceac48f13cec0860ab1acf5f"
+  },
+  {
+    "url": "static/media\\Symbola.cb8d804a.woff2",
+    "revision": "cb8d804a242b86175fdd6cb8e11b1a35"
+  },
+  {
+    "url": "static/media\\Symbola.e4ae9ff7.eot",
+    "revision": "e4ae9ff7ac2476ae421fc4278e5d3806"
+  },
+  {
+    "url": "workbox-sw.prod.v2.0.0.js.map",
+    "revision": "ae41383395d43fd4c5b9678d3010a498"
+  }
+];
+
+const workboxSW = new self.WorkboxSW();
+workboxSW.precache(fileManifest);
