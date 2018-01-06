0c5f4c1c6f8ef08064c78b39c9e755bf5419cb1c
pansitwattana <pansit.wt@gmail.com>
2018-01-06 17:22:48 +0700

    update new version

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('workbox-sw.prod.v2.0.0.js');
+importScripts('workbox-sw.prod.v2.1.1.js');
 
 /**
  * DO NOT EDIT THE FILE MANIFEST ENTRY
@@ -20,7 +20,7 @@ importScripts('workbox-sw.prod.v2.0.0.js');
 const fileManifest = [
   {
     "url": "asset-manifest.json",
-    "revision": "2ce69c2d08863f0fe09db7259287fd4b"
+    "revision": "b6be2a1b3712ef6ae28c271f008bce77"
   },
   {
     "url": "icon.png",
@@ -32,7 +32,7 @@ const fileManifest = [
   },
   {
     "url": "index.html",
-    "revision": "0607508ca687caae6be57a01ea2fc1bc"
+    "revision": "e68da826da6e2f68f2a0fce734d84325"
   },
   {
     "url": "jquery.min.js",
@@ -43,39 +43,39 @@ const fileManifest = [
     "revision": "6d7c488d83ab305a43ec411f7e13a009"
   },
   {
-    "url": "mathquill/font\\Symbola-basic.eot",
+    "url": "mathquill/font/Symbola-basic.eot",
     "revision": "8df3e41fee3876159737c311ed9b9642"
   },
   {
-    "url": "mathquill/font\\Symbola-basic.ttf",
+    "url": "mathquill/font/Symbola-basic.ttf",
     "revision": "c91d1d52217bb3429936c002ef6df9ed"
   },
   {
-    "url": "mathquill/font\\Symbola-basic.woff",
+    "url": "mathquill/font/Symbola-basic.woff",
     "revision": "d9f7c9e7d73b84f16563abacddbf1482"
   },
   {
-    "url": "mathquill/font\\Symbola-basic.woff2",
+    "url": "mathquill/font/Symbola-basic.woff2",
     "revision": "5e4d2a2f191cea67bcb285576286adbf"
   },
   {
-    "url": "mathquill/font\\Symbola.eot",
+    "url": "mathquill/font/Symbola.eot",
     "revision": "e4ae9ff7ac2476ae421fc4278e5d3806"
   },
   {
-    "url": "mathquill/font\\Symbola.otf",
+    "url": "mathquill/font/Symbola.otf",
     "revision": "4621fcfd9def63c694914f7ec5add610"
   },
   {
-    "url": "mathquill/font\\Symbola.ttf",
+    "url": "mathquill/font/Symbola.ttf",
     "revision": "52a6aac18ae26b6ecbd4f3a0d9579c9f"
   },
   {
-    "url": "mathquill/font\\Symbola.woff",
+    "url": "mathquill/font/Symbola.woff",
     "revision": "72981090d0240678c5d0a964fe29f082"
   },
   {
-    "url": "mathquill/font\\Symbola.woff2",
+    "url": "mathquill/font/Symbola.woff2",
     "revision": "2bb0421d8c622fb2aa1632b099807402"
   },
   {
@@ -87,55 +87,55 @@ const fileManifest = [
     "revision": "b96a12cc1530d084417bd4b4c050f6e3"
   },
   {
-    "url": "static/css\\main.71c41aee.css",
+    "url": "static/css/main.71c41aee.css",
     "revision": "98dc3b53fbbe5eb5292b5f99c5756c9c"
   },
   {
-    "url": "static/js\\main.bf96e8f3.js",
-    "revision": "1b31e1c3da4735521a0416c7809947f3"
+    "url": "static/js/main.cf336c59.js",
+    "revision": "8403589b7fca3aa29af1b6a9b9c902f1"
   },
   {
-    "url": "static/media\\flags.9c74e172.png",
+    "url": "static/media/flags.9c74e172.png",
     "revision": "9c74e172f87984c48ddf5c8108cabe67"
   },
   {
-    "url": "static/media\\icons.674f50d2.eot",
+    "url": "static/media/icons.674f50d2.eot",
     "revision": "674f50d287a8c48dc19ba404d20fe713"
   },
   {
-    "url": "static/media\\icons.912ec66d.svg",
+    "url": "static/media/icons.912ec66d.svg",
     "revision": "912ec66d7572ff821749319396470bde"
   },
   {
-    "url": "static/media\\icons.af7ae505.woff2",
+    "url": "static/media/icons.af7ae505.woff2",
     "revision": "af7ae505a9eed503f8b8e6982036873e"
   },
   {
-    "url": "static/media\\icons.b06871f2.ttf",
+    "url": "static/media/icons.b06871f2.ttf",
     "revision": "b06871f281fee6b241d60582ae9369b9"
   },
   {
-    "url": "static/media\\icons.fee66e71.woff",
+    "url": "static/media/icons.fee66e71.woff",
     "revision": "fee66e712a8a08eef5805a46892932ad"
   },
   {
-    "url": "static/media\\Symbola.4621fcfd.otf",
+    "url": "static/media/Symbola.4621fcfd.otf",
     "revision": "4621fcfd9def63c694914f7ec5add610"
   },
   {
-    "url": "static/media\\Symbola.52a6aac1.ttf",
+    "url": "static/media/Symbola.52a6aac1.ttf",
     "revision": "52a6aac18ae26b6ecbd4f3a0d9579c9f"
   },
   {
-    "url": "static/media\\Symbola.b1445a46.woff",
+    "url": "static/media/Symbola.b1445a46.woff",
     "revision": "b1445a46ceac48f13cec0860ab1acf5f"
   },
   {
-    "url": "static/media\\Symbola.cb8d804a.woff2",
+    "url": "static/media/Symbola.cb8d804a.woff2",
     "revision": "cb8d804a242b86175fdd6cb8e11b1a35"
   },
   {
-    "url": "static/media\\Symbola.e4ae9ff7.eot",
+    "url": "static/media/Symbola.e4ae9ff7.eot",
     "revision": "e4ae9ff7ac2476ae421fc4278e5d3806"
   }
 ];
